0d82b090a33dcb56dab85e315b9725d1ab292191
Jason A. <you@example.com>
2023-09-21 00:26:11 -0300

    adjust button check object size

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function toggleCheck(button) {
   button.classList.toggle("checked"); // Adiciona ou remove a classe "checked" do botão
+
+
+  const listItem = button.parentNode; // Obtém o elemento pai (o <li>)
+
+  // Alterna a classe "checked" no elemento pai (o <li>)
+  listItem.classList.toggle("checked");
 }
-
-
-
-
 
 
 
